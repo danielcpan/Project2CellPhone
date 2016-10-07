569b4385dab80f3dd2fb72c62ea2f38b934291dc
DanielKRing1 <DanielKRing1@gmail.com>
2016-10-07 13:26:29 -0700

    PhoneBook incomplete
    incomplete phonebook, use just as starting point

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,3 +1,28 @@
-public class PhoneBook{
-	int number;
+import java.util.ArrayList;
+
+public class PhoneBook {
+	private static ArrayList<Contact> contacts = new ArrayList<Contact>();
+	private static ArrayList<Favorite> favorites = new ArrayList<Favorite>();
+
+	public PhoneBook() {
+
+	}
+
+	public static void addContact(Contact userContact) {
+		contacts.add(userContact);
+	}
+
+	public static void addFavorite(Contact userFavorite) {
+		favorites.add(userFavorite);
+	}
+
+	public int getFavoriteSize() {
+		return favorites.size();
+	}
+	
+	public ArrayList<Contact> getContactArrayList(){
+		return contacts;
+	}
+	
+	public ArrayList
 }
